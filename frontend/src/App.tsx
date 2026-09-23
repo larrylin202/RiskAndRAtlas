558a37b08e95f6f769bb558e7c67b3e0de3970d5
larrylin202 <larrylin202@gmail.com>
2026-09-22 17:37:07 -0700

    Cleaned up HTML and CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import HazardMap from './components/map/HazardMap';
 
 export const App: React.FC = () => {
   return (
-    <main style={{ margin: 0, padding: 0, height: '100vh', width: '100vw', overflow: 'hidden' }}>
+    <main style={{ margin: 0, padding: 0, height: '100%', width: '100%', overflow: 'hidden' }}>
       <HazardMap />
     </main>
   );
